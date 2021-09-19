65e3740a17a3ba063cb89f77ac22a1272c233f22
Vitor Nilson de Oliveira Barbosa <vitornilson1998@gmail.com>
2021-09-19 19:45:56 -0300

    Testando chamada a fazenda SP

--- src/main/java/com/example/demo/controller/MailSenderController.java
+++ src/main/java/com/example/demo/controller/MailSenderController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import br.com.swconsultoria.certificado.exception.CertificadoException;
+import br.com.swconsultoria.nfe.exception.NfeException;
+import br.com.swconsultoria.nfe.schema_4.retConsSitNFe.TRetConsSitNFe;
 import com.example.demo.mod.SendMailDTO;
 import com.example.demo.service.EmailService;
 import org.apache.coyote.Response;
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 @RestController
@@ -30,6 +35,12 @@ public class MailSenderController {
     @RequestMapping(method = RequestMethod.POST, path = "/sendmail")
     public void sendMail(@RequestBody SendMailDTO sendMailDTO){
             emailService.sendSimpleMessage(sendMailDTO);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, path = "/findnfce")
+    public TRetConsSitNFe findNFCE() throws NfeException, CertificadoException {
+        return emailService.consultaNFce();
     }
 }
 
--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -1,7 +1,19 @@
 package com.example.demo.service;
 
+import br.com.swconsultoria.certificado.Certificado;
+import br.com.swconsultoria.certificado.exception.CertificadoException;
+import br.com.swconsultoria.nfe.Nfe;
+import br.com.swconsultoria.nfe.dom.ConfiguracoesNfe;
+import br.com.swconsultoria.nfe.dom.enuns.AmbienteEnum;
+import br.com.swconsultoria.nfe.dom.enuns.DocumentoEnum;
+import br.com.swconsultoria.nfe.dom.enuns.EstadosEnum;
+import br.com.swconsultoria.nfe.exception.NfeException;
+import br.com.swconsultoria.nfe.schema_4.retConsSitNFe.TRetConsSitNFe;
+import br.com.swconsultoria.nfe.util.NFCeUtil;
+import br.com.swconsultoria.nfe.wsdl.NFeConsultaProtocoloMS.NFeConsultaProtocolo4Stub;
 import com.example.demo.mod.SendMailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -23,5 +35,14 @@ public class EmailService{
         emailSender.send(message);
 
     }
+
+    public TRetConsSitNFe consultaNFce() throws CertificadoException, NfeException {
+        Certificado certificado = new Certificado();
+        String chave = "35210808385169000119590001294641606301830190";
+        ConfiguracoesNfe configuracoesNfe = ConfiguracoesNfe.criarConfiguracoes(EstadosEnum.SP, AmbienteEnum.PRODUCAO, certificado, "NFCE" );
+        TRetConsSitNFe retorno = Nfe.consultaXml(configuracoesNfe, chave, DocumentoEnum.NFCE);
+        return retorno;
+    }
+
 
 }
